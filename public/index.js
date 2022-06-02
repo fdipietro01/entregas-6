e31b2cd325a0f958dc0a87e4a553d4042822df6d
Flavio Di Pietro <fdipietro@santandertecnologia.com.ar>
2022-06-02 18:19:16 -0300

    attemp#2

--- public/index.js
+++ public/index.js
@@ -11,23 +11,68 @@ const setTableProducts = (data) => {
   document.getElementById("table").innerHTML = html;
 };
 
-socket.on("launchApp", async (productos) => {
-   const resp = await fetch("http://localhost:8080/table.hbs")
-   const answ = await resp.json().then(data => {console.log(data)})
-   console.log(resp)
-   console.log(answ)
-/*   template = Handlebars.compile(res);
-  setTableProducts(productos); */
+socket.on("launchApp", ({ products, chatHistory }) => {
+  /*   initializingTable(productos) */
+  initializingChat(chatHistory);
 });
 
 socket.on("updateTable", (productos) => {
   setTableProducts(productos);
 });
 
-document.getElementById("formulario").addEventListener("submit", (e) => {
-  e.preventDefault();
+socket.on("updateChat", (chatHistory) => initializingChat(chatHistory));
+
+socket.on("RegisterOk", (confirmation) => {
+  if (confirmation) {
+    document.getElementById("message-btn").removeAttribute("disabled");
+  }
+});
+
+const initializingTable = async (productos) => {
+  const resp = await fetch("http://localhost:8080/table.hbs");
+  const answ = await resp.json().then((data) => {
+    console.log(data);
+  });
+  console.log(resp);
+  console.log(answ);
+  template = Handlebars.compile(answ);
+  setTableProducts(productos);
+};
+
+const initializingChat = (chatHistory) => {
+  if (chatHistory.length === 0) {
+    const doc = document.createElement("p")
+    doc.innerHTML = "No messages yet";
+    doc.classList.add("input-group-text", "text-danger")
+    document.getElementById("chat").appendChild(doc);
+  } else {
+    chatHistory.forEach((element) => {
+      const doc = (document.createElement("p").innerHTML = element);
+      document.getElementById("chat").appendChild(doc);
+    });
+  }
+};
+
+const sendMessage = () => {};
+
+const newProduct = () => {
   const name = document.getElementById("name").value;
   const price = document.getElementById("price").value;
   const img = document.getElementById("img").value;
   socket.emit("newProduct", { name, price, img });
-});
+  return false;
+};
+
+const newMail = () => {
+  console.log("entro por acá");
+  const mail = document.getElementById("mail").value;
+  socket.emit("newMail", mail);
+  return false;
+};
+
+const setNewMessage = () => {
+  const text = document.getElementById("message").value;
+  const date = new Date().toLocaleString();
+  socket.emit("newMessage", { text, date });
+  return false;
+};
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const Contenedor = require("./contenedor/contenedor");
 const { Server: HttpServer } = require("http");
 const { Server: SocketServer } = require("socket.io");
 
+const chatHistory = []
 const contenedor = new Contenedor("Mi Contenedor");
 
 app.use(express.static("public"));
@@ -16,8 +17,27 @@ const httpServer = new HttpServer(app);
 const socketServer = new SocketServer(httpServer);
 
 socketServer.on("connection", (socket) => {
-  socket.emit("launchApp", contenedor.getProducts());
+  socket.emit("launchApp", {products: contenedor.getProducts(), chatHistory});
 
+  // listener para registrar nuevo correo y confirmarlo
+  socket.on("newMail", (mail)=>{
+    console.log("nuevo correo", mail)
+    socket.email = mail;
+    socket.emit("RegisterOk", true)
+  })
+
+  // listener para añadir mensaje a la colección y actualizar el histial
+  socket.on("newMessage", ({text, date}) => {
+    const message = `<p class="input-group-text">
+    <span class="text-primary font-weight-bold">${socket.mail}</span>
+    <span class="text-danger>${date}</span>:
+    <span class="text-success font-italic>${text}</span>
+ </p>`;
+    chatHistory.push(message);
+    socketServer.sockets.emit("updateChat", chatHistory);
+  })
+
+  // listener para añadir producto a la colección y actualizar la tabla
   socket.on("newProduct", (producto) => {
     contenedor.addItem(producto);
     socketServer.sockets.emit("updateTable", contenedor.getProducts());
